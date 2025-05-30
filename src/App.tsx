81ed37519ea2c2125d6106723117744dd6486792
MemePulse2024 <kavanhinboo@gmail.com>
2025-05-30 09:55:31 +0000

    asssddd

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,12 @@ const App = () => (
         </Routes>
       </BrowserRouter>
       {/* PulseNet Watermark - always floating bottom left */}
-      <div
+      <img
+        src="/PulseNet-watermark.svg"
+        alt="PulseNet Watermark (debug: should see SVG here)"
         className="pulsenet-watermark"
+        draggable="false"
         aria-hidden="true"
-        style={{ position: 'fixed', left: '0.5rem', bottom: '0.5rem', zIndex: 99999, width: 120, height: 'auto', pointerEvents: 'none', userSelect: 'none' }}
-        dangerouslySetInnerHTML={{ __html: `
-<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>
-  <path d='M55.45 c-6.80 -1.40 -11.50 -3.5' fill='none' stroke='#FF9900' stroke-width='2' />
-</svg>
-` }}
       />
     </TooltipProvider>
   </QueryClientProvider>
